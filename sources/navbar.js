81a4948fb24b65e5dc4cb6d13b3c37aaad28218b
unknown <babic.goran@outlook.com>
2020-12-22 01:12:25 +0100

    DESTINACIJE, DESTINACIJA - ugradjen navbar (animacija)

---
+++ sources/navbar.js
@@ -0,0 +1,7 @@
+document.write('\
+    <nav class="navigation-bar col-12" id="gradient">\
+        <a href="">Home</a>\
+        <a href="">Destinacije</a>\
+        <a href="">Kontakt</a>\
+    </nav>\
+')
