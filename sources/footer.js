d88290afa2fb11b29821b9e187d9aacef0bd1eb7
unknown <babic.goran@outlook.com>
2021-01-02 21:47:00 +0100

    FOOTER - gradient, sitni bagovi, novi button na naslovnoj

--- sources/footer.js
+++ sources/footer.js
@@ -1,5 +1,5 @@
 document.write('\
-    <footer>\
+    <footer class="gradient">\
         <a href="https://www.instagram.com/putuj.fotkaj.pisi/" target="_blank" rel="noreferrer">\
             <img src="https://www.putujfotkajpisi.rs/images/instagram.png" alt="instagram-logo">\
         </a>\
